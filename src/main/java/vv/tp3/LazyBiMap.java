7d98b0c28ed71188292efb61b0322dc77e0cbc12
amandine <didinem2@gmail.com>
2014-10-15 21:20:27 +0200

    update

--- src/main/java/vv/tp3/LazyBiMap.java
+++ src/main/java/vv/tp3/LazyBiMap.java
@@ -8,4 +8,13 @@ public class LazyBiMap<K,V> extends BiMap<K,V> {
 
     public LazyBiMap(Factory<K, V> keyFactory, Factory<V, K> valueFactory) {
    }
+    //TODO
+    public V get(Object key){
+        if (k.contains(key)) {
+            return v.get(k.indexOf(key));
+        }else{
+            return put((K)key, (V)key);
+
+        }
+    }
 }
