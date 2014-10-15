70a9a5ee037a63535a8e407fd71e0000975fcb41
amandine <didinem2@gmail.com>
2014-10-15 18:29:38 +0200

    update

--- src/main/java/vv/tp3/BiMap.java
+++ src/main/java/vv/tp3/BiMap.java
@@ -2,7 +2,9 @@ package vv.tp3;
 
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BiMap<K,V> extends SimpleMap<K,V> {
 
@@ -47,6 +49,12 @@ public class BiMap<K,V> extends SimpleMap<K,V> {
         return null;
     }
 
+    @Override
+    public boolean containsKey(Object key) {
+        return k.contains(key);
+    }
+
+    @Override
     public boolean containsValue(Object value) {
         return v.contains(value);
     }
@@ -55,5 +63,53 @@ public class BiMap<K,V> extends SimpleMap<K,V> {
         if (k.contains(key))
             return v.get(k.indexOf(key));
         return null;
+    }
+
+    @Override
+    public V remove(Object key) {
+        V before;
+        if(k.contains(key)){
+            before = get(key);
+            v.remove(k.indexOf(key));
+            k.remove(key);
+            return before;
+        }
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return k.size();
+    }
+
+    @Override
+    public void clear() {
+        k.clear();
+        v.clear();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return k.isEmpty();
+    }
+
+    public boolean equals(Object o){
+        if(o instanceof SimpleMap){
+            SimpleMap map = (SimpleMap) o;
+            if(v.equals(map.v) && k.equals(map.k)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int hashCode(){
+        return k.hashCode()+v.hashCode();
+    }
+
+    @Override
+    public Set<K> keySet() {
+
+        return new HashSet<K>(k);
     }
 }
