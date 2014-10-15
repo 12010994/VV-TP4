b075c510e7b18c296a5591589e3a43985418e2d8
vpetit <victor.petit@etudiant.univ-rennes1.fr>
2014-10-15 11:47:49 +0200

    first commit

---
+++ src/main/java/vv/tp3/SimpleMap.java
@@ -0,0 +1,88 @@
+package vv.tp3;
+
+import vv.tp3.factory.Doublet;
+
+import java.util.*;
+
+public class SimpleMap<K,V> implements Map<K,V> {
+
+    List<K> k;
+    List<V> v;
+
+    public SimpleMap(){
+        k = new ArrayList<K>();
+        v = new ArrayList<V>();
+    }
+
+
+
+
+    @Override
+    public int size() {
+        return k.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public V get(Object key) {
+        return v.get(k.indexOf(key));
+    }
+
+    @Override
+    public V put(K key, V value){
+        V before;
+        if(k.contains(key)){
+            k.add(k.indexOf(key),key);
+            before = v.get(k.indexOf(key));
+            v.add(k.indexOf(key),value);
+            return before;
+        }
+        k.add(key);
+        v.add(value);
+        return null;
+    }
+
+    @Override
+    public V remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection<V> values() {
+        return null;
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return null;
+    }
+}
---
+++ src/main/java/vv/tp3/factory/Doublet.java
@@ -0,0 +1,15 @@
+package vv.tp3.factory;
+
+/**
+ * Created by vpetit on 15/10/14.
+ */
+public class Doublet<K,V> {
+
+    public K k;
+    public V v;
+
+    public Doublet(K key, V value){
+        k=key;
+        v=value;
+    }
+}
