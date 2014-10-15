d200874dc1545f8e12e65b2d0bd4709addc713dd
amandine <didinem2@gmail.com>
2014-10-15 17:59:30 +0200

    update

--- src/main/java/vv/tp3/BiMap.java
+++ src/main/java/vv/tp3/BiMap.java
@@ -1,15 +1,59 @@
 package vv.tp3;
 
-/**
- * Created by Simon on 30/09/14.
- */
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BiMap<K,V> extends SimpleMap<K,V> {
 
-    public K getByValue(Object value) {
-        return null;
+    List<K> k;
+    List<V> v;
+
+    public BiMap(){
+        k = new ArrayList<K>();
+        v = new ArrayList<V>();
+    }
+
+    @Override
+    public V put(K key, V value){
+
+        if(k.contains(key)){
+            removeValue(key);
+        }
+        if (v.contains(value)){
+            removeValue(k.get(v.indexOf(value)));
+        }
+        k.add(key);
+        v.add(value);
+
+        return value;
     }
 
     public K removeValue(Object key) {
+        K before;
+        if(k.contains(key)){
+            before = k.get(k.indexOf(key));
+            v.remove(k.indexOf(key));
+            k.remove(key);
+            return before;
+        }
+        return null;
+
+    }
+
+    public K getByValue(Object value) {
+        if (v.contains(value))
+            return k.get(v.indexOf(value));
+        return null;
+    }
+
+    public boolean containsValue(Object value) {
+        return v.contains(value);
+    }
+
+    public V get(Object key) {
+        if (k.contains(key))
+            return v.get(k.indexOf(key));
         return null;
     }
 }
